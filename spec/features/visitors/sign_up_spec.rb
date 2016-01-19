908d014e4f77e663f53c235248e6f46c8c1275cd
Roman Pushkin <roman.pushkin@gmail.com>
2016-01-18 19:26:39 -0800

    Fix broken sign up test

--- spec/features/visitors/sign_up_spec.rb
+++ spec/features/visitors/sign_up_spec.rb
@@ -35,10 +35,11 @@ feature 'Sign Up', :devise do
   # Scenario: Visitor cannot sign up with a short password
   #   Given I am not signed in
   #   When I sign up with a short password
-  #   Then I see a 'too short password' message
-  scenario 'visitor cannot sign up with a short password' do
-    sign_up_with('test@example.com', 'please', 'please')
-    expect(page).to have_content "Password is too short"
+  #   Then I see a successful sign up message
+  scenario 'visitor can sign up with a short password' do
+    sign_up_with('test@example.com', '1', '1')
+    txts = [I18n.t( 'devise.registrations.signed_up'), I18n.t( 'devise.registrations.signed_up_but_unconfirmed')]
+    expect(page).to have_content(/.*#{txts[0]}.*|.*#{txts[1]}.*/)
   end
 
   # Scenario: Visitor cannot sign up without password confirmation
